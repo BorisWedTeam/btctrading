06d1331d23c36343714777aa8bedb4e74bd50f53
ggoldong <dev.pros17@gmail.com>
2017-11-03 18:44:12 -0700

    other 2pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'BTC' });
+});
+
+/* GET news page page. */
+router.get('/news', function(req, res, next) {
+    res.render('news', { title: 'BTC' });
+});
+
+/* GET tokens page page. */
+router.get('/tokens', function(req, res, next) {
+    res.render('tokens', { title: 'BTC' });
 });
 
 module.exports = router;
